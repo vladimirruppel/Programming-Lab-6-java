06005f950c049920e789ceee7fa6b00f4289c415
vladimirruppel <vladimir.ruppel2004@mail.ru>
2023-12-26 15:59:47 +0700

    Добавление логики работы в класс Album

--- src/Album.java
+++ src/Album.java
@@ -16,5 +16,37 @@ public class Album extends TrackList {
         this.albumName = albumName;
     }
 
+    // сеттеры
+    public void SetAlbumName(String albumName) throws IllegalArgumentException {
+        if (albumName.isEmpty()) throw new IllegalArgumentException("Пустое название альбома недопустимо");
+        this.albumName = albumName;
+    }
 
+    public void SetArtistName(String artistName) {
+        if (artistName.isEmpty()) throw new IllegalArgumentException("Пустое имя артиста недопустимо");
+        this.artistName = artistName;
+    }
+
+    // геттеры
+    public String GetAlbumName() {
+        return albumName;
+    }
+
+    public String GetArtistName() {
+        return artistName;
+    }
+
+    public String GetAlbumInfo() {
+        return artistName + " - " + albumName + ", " + super.GetSize() + " треков";
+    }
+
+    // методы
+    public void PrintInfoShort() {
+        System.out.println(GetAlbumInfo());
+    }
+
+    public void PrintInfoLong() {
+        System.out.println(GetAlbumInfo() + ": ");
+        super.PrintTrackList();
+    }
 }
